9d5ae5b8ade752b7f4a3cdc1b7f29c65979bfd87
Trolann <trevor@mathisen.dev>
2023-11-02 20:08:38 -0700

    getSettings started

--- packages/auth/supa_auth/fake_settings.py
+++
@@ -1,9 +0,0 @@
-fake_settings_json = {
-    "nickname": "fake",
-    "dark_mode": True,
-    "language": "en",
-    "vegan": False,
-    "friends": [
-        12345, 67890, 13579, 24680, 98765, 43210
-    ]
-}
