0406974d5607e01c8adfd6b895f45ff96c88f24a
Trolann <trevor@mathisen.dev>
2023-12-04 06:32:30 -0800

    getsessioninfo updates

--- test_new_session.py
+++ test_new_session.py
@@ -1,17 +1,15 @@
 import requests
 from get_auth import get_access_token
-url = "https://sea-lion-app-s86sj.ondigitalocean.app/session/newSession"
-payload = {
-    'lat': 37.334665328,
-    'long': -121.875329832,
-    'filter_distance': 10000,
+url = "https://sea-lion-app-s86sj.ondigitalocean.app/session/getSessionInfo"
+args = {
     'user_id': '71f87b7c-55bf-488d-a562-7cd8e120495d',
-    "access_token": get_access_token()
+    "access_token": get_access_token(),
+    "session_id": "88"
 }
 headers = {
     "Content-Type": "application/json"
 }
 
 if __name__ == '__main__':
-    response = requests.get(url, json=payload, headers=headers)
+    response = requests.get(url, json=args, headers=headers)
     print(response.text)
